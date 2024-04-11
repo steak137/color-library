bc8b0050f4e7da015bf537195a97dcc09fbf7749
watan beck <96546018+steak137@users.noreply.github.com>
2024-04-11 04:29:47 +0300

    Update color.h

--- color/color/color.h
+++ color/color/color.h
@@ -5,17 +5,17 @@
 
 #ifndef __COLOR_H__
 #include <fstream>
-#include <fmt/format.h>
+#include <format.h>
 
 class color {
 public:
 	color(int r, int g, int b) : r_(r), g_(g), b_(b) {}
 	friend std::ostream& operator<<(std::ostream& os, const color& color) {
-		return os << fmt::format("\x1B[38;2;{};{};{}m", color.r_, color.g_, color.b_);
+		return os << std::format("\x1B[38;2;{};{};{}m", color.r_, color.g_, color.b_);
 	}
 	~color() {}
 	friend std::ostream& operator<<(std::ostream& os) {
-		return os << fmt::format("\x1B[38;2;255;255;255m");
+		return os << std::::format("\x1B[38;2;255;255;255m");
 	}
 private:
 	int r_, g_, b_;
